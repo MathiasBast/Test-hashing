6aaa332a7125c6640f1bb50c0f13c66f7f3d8cca
Mathias Bast <mathiasbast123@gmail.com>
2020-04-05 16:11:14 +1200

    inital commit

---
+++ routes.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const router = express.Router()
+const bcrypt = require('bcrypt')
+const saltRounds = 10
+const myData = require('./data.json')
+const utils = require('./utils')
+
+router.get('/', (req, res) => {
+  res.render('home', myData)
+})
+
+router.get('/add', (req, res) => {
+  res.render('add', myData)
+})
+
+router.post('/add', (req, res) => {
+  const { name, age, qualites, password } = req.body
+  let data = {
+    id: myData.users.length + 1,
+    name,
+    age,
+    qualites,
+    password
+  }
+  bcrypt.hash(data.password, saltRounds, function (err, hash) {
+    if (err) console.error(err)
+    data.password = hash
+    utils.add(data, err => {
+      if (err) return res.sendStatus(500)
+      res.redirect('/add')
+    })
+  })
+})
+
+router.get('/see_passwords', (req, res) => {
+  res.render('passwords')
+})
+
+router.post('/see_passwords', (req, res) => {
+  const { name, password } = req.body
+  let data = {
+    name,
+    password
+  }
+  utils.checkPassword(data, (err, data) => {
+    if (err) {
+      data = {
+        error: "Can not find Username or Password"
+      }
+      res.render('passwords', data)
+    } else {
+      res.redirect('/')
+    }
+  })
+})
+
+module.exports = router
+
---
+++ utils.js
@@ -0,0 +1,49 @@
+const fs = require('fs')
+const path = require('path')
+const bcrypt = require('bcrypt')
+
+module.exports = {
+  add,
+  checkPassword
+}
+
+function add (myData, callback) {
+  const fileName = path.join(__dirname, 'data.json')
+  fs.readFile(fileName, (err, contents) => {
+    if (err) return new Error('can not load')
+    const json = JSON.parse(contents)
+    json.users.push(myData)
+    const string = JSON.stringify(json, null, 2)
+    fs.writeFile(fileName, string, 'utf-8', callback)
+  })
+}
+
+function checkPassword (data, callback) {
+  const fileName = path.join(__dirname, 'data.json')
+  fs.readFile(fileName, (err, contents) => {
+    if (err) return new Error('can not load')
+    const json = JSON.parse(contents)
+    var indx = null
+    json.users.forEach(element => {
+      if (element.name === data.name) {
+        indx = element.id - 1
+      }
+    })
+    if (indx !== null) {
+      const HashPassword = json.users[indx].password
+      bcrypt.compare(data.password, HashPassword, function (err, result) {
+        if (err) Error('not working at hashing')
+        if (result) {
+          callback(null, result)
+        } else {
+          var aErr = new Error('User Name or password not found')
+          callback(aErr, null)
+        }
+      })
+    } else {
+      var aErr = new Error('User Name or password not found')
+      callback(aErr, null)
+    }
+  })
+}
+
